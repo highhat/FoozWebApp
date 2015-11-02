c49c86d330c8772dc9418a508dbc70beecc6090f
Entremont, David <dentremont@DELOITTE.com>
2015-11-02 17:12:39 -0500

    Init with express

---
+++ app.js
@@ -0,0 +1,22 @@
+// Import dependencies
+var express = require('express');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+// Initialize app
+var app = express();
+
+// Define running port
+app.set('port', (process.env.PORT || 5000));
+
+// Initialize plugins
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+// Run app
+app.listen(app.get('port'), function() {
+  console.log('Fooz is running on port', app.get('port'));
+});
+
+module.exports = app;

438eca1261219c2f5e7a047242cd738cab4fe040
Entremont, David <dentremont@DELOITTE.com>
2015-11-11 00:11:35 -0700

    API update

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
+var mongo = require('mongodb');
+var monk = require('monk');
+
+var mongoUri = process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://fooz_prvt:1400market@ds049754.mongolab.com:49754/heroku_1vjw0wrr'; 
+var db = monk(mongoUri);
 
 // Load page for login
 router.post('/user/:id/update', function(req, res) {
-	res.send(req.body);
+	
+	// Query user record by user id
+	var users = db.get('game_stats');
+	users.find({ userId: req.params.id }, function (err, docs) {
+		res.send('Done' + err + docs);
+	});
 });
 
 

8b280f3498ccf8ca8cc616b82955ef6f38bef1e5
Entremont, David <dentremont@DELOITTE.com>
2015-11-11 00:24:24 -0700

    Added mongoose

--- routes/api.js
+++ routes/api.js
@@ -1,17 +1,25 @@
 var express = require('express');
 var router = express.Router();
 var mongo = require('mongodb');
-var monk = require('monk');
-
+var mongoose = require('mongoose');
+ 
 var mongoUri = process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://fooz_prvt:1400market@ds049754.mongolab.com:49754/heroku_1vjw0wrr'; 
-var db = monk(mongoUri);
+mongoose.connect(mongoUri);
+
+var userSchema = new mongoose.Schema({
+	userId: String,
+	email: String,
+	name: String,
+	avatarURL: String,
+	access_token: String,
+	score: Number
+});
+
+var User = mongoose.model('game_stats', userSchema);
 
 // Load page for login
 router.post('/user/:id/update', function(req, res) {
-	
-	// Query user record by user id
-	var users = db.get('game_stats');
-	users.find({ userId: req.params.id }, function (err, docs) {
+	User.findOne({ 'userId': req.params.id }, function (err, docs) {
 		res.send('Done' + err + docs);
 	});
 });
